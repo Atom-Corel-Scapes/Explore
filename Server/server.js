8b2206a60352bbee25529f089fb76973e74cb437
SRIRAM-D-B <sriramdevarajulu@gmail.com>
2022-09-10 09:07:27 -0700

    connected Mongo Atlas -Sriram

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,17 @@
 import express from "express";
+import mongoose from "mongoose";
 import { Port } from "./Const.js";
+import { mongoUrl } from "./Const.js";
 
 const app = express();
+
+mongoose.connect(mongoUrl);
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open",  () => {
+  console.log("Database Connected successfully");
+});
 
 app.listen(Port, () => {
   console.log(`Server listening at ${Port}`);
