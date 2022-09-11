2d8d20d50f542d29b11b10c039b8536eea4dc1b4
SRIRAM-D-B <sriramdevarajulu@gmail.com>
2022-09-11 13:28:39 -0700

    created server -sriram

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,15 @@
 import express from "express";
 import { Port } from "./Const.js";
+import { mongoUrl } from "./Const.js";
+import mongoose from "mongoose";
 
 const app = express();
+
+mongoose
+  .connect(mongoUrl)
+  .then(() => console.log("Database Connection successfully!!!"))
+  .catch((err) => console.log("Database Connection Failed!!!", err.message));
+
 
 app.listen(Port, () => {
   console.log(`Server listening at ${Port}`);
