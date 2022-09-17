9dba030c489176995947c4cd81a3f39f5e7e5218
SRIRAM-D-B <sriramdevarajulu@gmail.com>
2022-09-16 21:03:02 -0700

    server-side signup page -Sriram

--- Server/Schema/UploadSchema.js
+++ Server/Schema/UploadSchema.js
@@ -1,4 +1,39 @@
 import mongoose from "mongoose";
 
-const uploadSchema = new mongoose.Schema();
-export const uploadModel = new mongoose.model("upload", uploadSchema);
+const userSchema = new mongoose.Schema(
+  {
+    userFirstname: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 30,
+    },
+    userLastname: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 30,
+    },
+    userEmail: {
+      type: String,
+      required: true,
+      minLength: 2,
+      maxLength: 60,
+      unique: true,
+    },
+    userPassword: {
+      type: String,
+      required: true,
+      min: 6,
+      max: 100,
+    },
+    userPhoneNumber: {
+      type: Number,
+      required: true,
+      minLength: 10,
+    },
+  },
+  { timestamps: true }
+);
+
+export const userModel = new mongoose.model("usersDatas", userSchema);
