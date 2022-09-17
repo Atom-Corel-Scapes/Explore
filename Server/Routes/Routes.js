48f86fcaa35c6a0aa14bf5223907b42d6d21d60d
SRIRAM-D-B <sriramdevarajulu@gmail.com>
2022-09-16 21:26:52 -0700

    server-side upload -sriram

--- Server/Routes/Routes.js
+++ Server/Routes/Routes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { signup } from "../Controllers/Signup.js";
 import { login } from "../Controllers/Login.js";
+import { uploadData } from "../Controllers/Upload.js";
 
 const Route = express.Router();
 
 Route.post("/signup", signup);
 Route.get("/login", login);
+Route.post("/upload", uploadData);
 
 export default Route;
---
+++ Server/Schema/UploadSchema.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+// upload Schema
+const uploadSchema = new mongoose.Schema(
+  {
+    placeName: {
+      type: String,
+      required: true,
+    },
+    placeTag: {
+      type: String,
+      requried: true,
+    },
+    placeDescription: {
+      type: String,
+      requried: true,
+    },
+    placeImage: {
+      data: Buffer,
+      contentType: String,
+    },
+  },
+  { timestamps: true }
+);
+export const uploadModel = new mongoose.model("upload", uploadSchema);
