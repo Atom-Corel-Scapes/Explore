ede8e013551295156e0e5287e16e19d3185159e1
JBSanjay <jbsanjaysanjay579@gmail.com>
2022-09-19 13:32:45 +0530

    Skeleton for DetailPage is added

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,6 +1,5 @@
 import React from "react"
 import './App.css';
-<<<<<<< HEAD
 import NavBar from './component/NavBar';
 import { Provider } from 'react-redux';
 import store from './Redux/Store/Store';
@@ -20,27 +19,16 @@ function App() {
           <BrowserRouter>
             <Routes>
               <Route path="/" element={<><NavBar/><Login /></>} exact ></Route>
-            
+             
               <Route path="/signup" element={<><NavBar /><Signup /></>}></Route>
             </Routes>
           </BrowserRouter>
         </Provider>
 
       </React.StrictMode>
-=======
-import NavBar from './component/navBar/NavBar';
-import Login from './component/Login/Login';
-
-
-function App() {
-    return (
-    <div className="App">
-        <NavBar />
-        <Login />
->>>>>>> 381ff121f816010acc2982b1562a136a8abf7a31
     </div>
   );
-}
+};
 
 export default App;
 
